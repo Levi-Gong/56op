55dcd9721bff674bd177061332aaa3f55f97e785
levi.gong <levi.gong@sinoservices.com>
2019-01-02 18:06:07 +0800

    add some i don't not

--- src/main/java/com/sinoservices/op/controller/HomeController.java
+++ src/main/java/com/sinoservices/op/controller/HomeController.java
@@ -10,9 +10,7 @@ import java.util.Map;
 public class HomeController {
   @GetMapping("/")
   public String home(Map<String, Object> model) {
-    model.put("message", "Hello World");
-    model.put("title", "Hello Home");
-    model.put("date", new Date());
-    return "home";
+
+    return "index.html";
   }
 }
